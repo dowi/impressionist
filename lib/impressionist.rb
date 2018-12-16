4ca945da29071da71b4b52faa33d31d3a549548e
Dovi Winberger <dowi84@gmail.com>
2018-12-16 12:46:48 +0200

    adding accessor

--- lib/impressionist.rb
+++ lib/impressionist.rb
@@ -3,6 +3,7 @@ require 'impressionist/load'
 module Impressionist
   # Define default ORM
   mattr_accessor :orm
+  mattr_accessor :user_function
   @@orm = :active_record
   @@user_function = nil
 
