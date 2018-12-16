5944f8ed79f816414f03f4913fe8dc0caed545c1
Dovi Winberger <dowi84@gmail.com>
2018-12-16 14:04:04 +0200

    global_exclude fix

--- lib/impressionist.rb
+++ lib/impressionist.rb
@@ -4,6 +4,7 @@ module Impressionist
   # Define default ORM
   mattr_accessor :orm
   mattr_accessor :user_function
+  mattr_accessor :global_exclude
   @@orm = :active_record
   @@user_function = nil
   @@global_exclude = []
